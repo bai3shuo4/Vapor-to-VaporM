67263d3e8db898be085dd142b0bab7d806e539d5
bai3shuo4 <bai3shuo4@gmail.com>
2017-11-26 22:20:08 -0800

    complete with most basic condition with only t

--- cs132/LinearScanAllocation.java
+++ cs132/LinearScanAllocation.java
@@ -15,17 +15,25 @@ public class LinearScanAllocation{
 	private VFunction v;
 	private HashMap<String, Interval> interval;
 
+	private final HashMap<String, String> register = new HashMap<String, String>();   // interval_name registername
+	private final HashMap<String, String> location = new HashMap<String, String>();
+
+	private final LinkedList<String> t_register = new LinkedList<>();
+	private final LinkedList<String> s_register = new LinkedList<>();
+
 	private final LinkedHashMap<String, Integer> startpoint = new LinkedHashMap<>();
 	private final LinkedHashMap<String, Integer> endpoint = new LinkedHashMap<>();
 
+	private LinkedList<String> active;      //store_active interval
 
-
-	private LinkedList<String, Integer> active;
-
+	int local;
+	HashMap<String, String> allocate_map;
 
 	public LinearScanAllocation(HashMap<String, Interval> interval){
 
 		this.interval = interval;
+		local = 0;
+		allocate_map = new HashMap<>();
 
 		List<Map.Entry<String, Interval>> list = new ArrayList<Map.Entry<String, Interval>>(interval.entrySet());
 		Collections.sort(list, new Comparator<Map.Entry<String, Interval>>() {
@@ -47,10 +55,17 @@ public class LinearScanAllocation{
 			endpoint.put(tmp.getKey(), tmp.getValue().end);
 		}
 
+		for(int i = 0; i < 8; i++){
+			t_register.add("$t" + Integer.toString(i));
+			s_register.add("$s" + Integer.toString(i));
+		}
+
+		t_register.add("$t8");
+
 
 	}
 
-	public void readmap(){
+	public void readMap(){
 
 		for(Map.Entry<String, Integer> tmp : startpoint.entrySet()){
 			System.out.print(tmp.getKey() + " ");
@@ -65,6 +80,33 @@ public class LinearScanAllocation{
 		}
 	}
 
+	public void getMap(){
+
+		for(Map.Entry<String, String> tmp : register.entrySet()){
+			allocate_map.put(tmp.getKey(),tmp.getValue());
+		}
+		for(Map.Entry<String, String> tmp : location.entrySet()){
+			allocate_map.put(tmp.getKey(),tmp.getValue());
+		}
+	}
+
+	public String findFreeRegister(){
+		String register_name = "error";
+
+		if(!t_register.isEmpty()){
+			register_name = t_register.getFirst();
+			t_register.removeFirst();
+		}
+
+		return register_name;
+	}
+
+	public void returnFreeRegister(String register_name){
+
+		t_register.add(0, register_name);    				//put it in the head
+
+	}
+
 
 
 	public void LinearScanRegisterAllocation(){
@@ -72,45 +114,65 @@ public class LinearScanAllocation{
 		active = new LinkedList<>();
 
 		for(Map.Entry<String, Integer> interval : startpoint.entrySet()){
-			ExpireOldIntervals(interval.getKey());
-			if(interval.size() == R)
-				SpillAtInterval(interval.getKey());
+			String i = interval.getKey();
+			ExpireOldIntervals(i);
+			if(active.size() == 9)					//considering only t register
+				SpillAtInterval(i);
 			else{
 				//register[i] <--- free register
+				register.put(i, findFreeRegister());
 				//i ---> active
+				active.add(i);
 				//sort active by end point
+				sortActive(active);
 			}
 		}
 	}
 
-	public void ExpireOldIntervals(String name){
+	public void ExpireOldIntervals(String i){
 
-		for(Map.Entry<String, Integer> interval : active.entrySet()){
-			if(endpoint.get(interval.getKey()) >= startpoint.get(name))
+		for(String j : active){
+			if(endpoint.get(j) >= startpoint.get(i))
 				return;
 			//remove j from active;
+			active.remove(j);
 			//add register[j] to pool of free register
+			returnFreeRegister(register.get(j));
 		}
 	}
 
-	public void SpillAtInterval(String name){
+	public void SpillAtInterval(String i){
 
 		String spill = active.getLast();
-		if(endpoint.get(spill) > endpoint.get(name)){
+		if(endpoint.get(spill) > endpoint.get(i)){
 			//register[i] <----- register[spill]
+			String register_spill = register.get(spill);
+			register.put(i, register_spill);
+			register.remove(spill);
 			//location[spill] <----- new stack location
+			location.put(spill, "local[" + Integer.toString(local) + "]");
+			local = local + 1;
 			//remove spill from active
+			active.remove(spill);
 			//add i to active, sort by increasing end point
+			active.add(i);
+			sortActive(active);
 		}
 		else{
-			//location <----- new stack location
+			//location[i] <----- new stack location
+			location.put(i, "local[" + Integer.toString(local) + "]");
+			local = local + 1;
 		}
 	}
 
+	public void sortActive(LinkedList<String> active){
+		Collections.sort(active, new Comparator<String>() {
+			public int compare(String o1, String o2) {
+				return (endpoint.get(o1) - endpoint.get(o2));
+			}
+		});
 
-
-
-
+	}
 
 
 }
--- cs132/V2VM.java
+++ cs132/V2VM.java
@@ -10,12 +10,12 @@ import java.io.*;
 
 
 public class V2VM extends VInstr.Visitor<Throwable>{
-			//static boolean find_out;
+  		//static boolean find_out;
 			//static int out_num;
-
+  static HashMap<String, String> allocate_map;
 	public static void main(String [] args) throws Throwable{
 
-		
+  		
 			Op[] ops = {
 			Op.Add, Op.Sub, Op.MulS, Op.Eq, Op.Lt, Op.LtS,
     		Op.PrintIntS, Op.HeapAllocZ, Op.Error,
@@ -26,72 +26,101 @@ public class V2VM extends VInstr.Visitor<Throwable>{
 			boolean allowStack = false;
 
 			VaporProgram program = null;
-		try{
-			
-    		program = VaporParser.run(new InputStreamReader(System.in), 1, 1,
-                              Arrays.asList(ops),
-                              allowLocals, registers, allowStack);
-  			}
+  		try{
+  			
+      		program = VaporParser.run(new InputStreamReader(System.in), 1, 1,
+                                Arrays.asList(ops),
+                                allowLocals, registers, allowStack);
+    			}
   		catch (Exception e) {
-  			e.printStackTrace();
-  			System.out.println("Error");
-  			//return;
-  		}
+    			e.printStackTrace();
+    			System.out.println("Error");
+    			//return;
+    		}
 
   		for(VDataSegment class_name : program.dataSegments){
-  			System.out.println("const " + class_name.ident);
-  			
-  			for(VOperand.Static function_name : class_name.values){
-  				System.out.println("	" + function_name.toString());
-  			}
-  			System.out.println();
+    			System.out.println("const " + class_name.ident);
+    			
+    			for(VOperand.Static function_name : class_name.values){
+    				System.out.println("	" + function_name.toString());
+    			}
+    			System.out.println();
 
-  		}
+    		}
 
+      V2VM v2vm = new V2VM();
   		for(VFunction function : program.functions){
-  			System.out.print(function.ident);
+      			 System.out.print("func " + function.ident);
 
-  			 int parameter_length = function.params.length;
-  			 if(parameter_length <= 4){
-  				System.out.print(" [in 0 ");
-  			 }
-  			 else{
-  			 	System.out.print(" [in " + Integer.toString(parameter_length - 4) + " ");
-  			 }
+      			 int parameter_length = function.params.length;
+      			 if(parameter_length <= 4){
+      				System.out.print(" [in 0, ");
+      			 }
+      			 else{
+      			 	System.out.print(" [in " + Integer.toString(parameter_length - 4) + ", ");
+      			 }
 
-  			 find_out out = new find_out(function);
-  			 out.findOutNum();
-  			 int out_num = out.out_num;
-  			 	if(out_num <= 4){
-  			 		System.out.print("out 0 ");
-  			 	}
-  			 	else{
-  			 		out_num = out_num - 4;
-  			 		System.out.print("out " + Integer.toString(out_num) + " ");
-  			 	}
+      			 find_out out = new find_out(function);
+      			 out.findOutNum();
+      			 int out_num = out.out_num;
+      			 	if(out_num <= 4){
+      			 		System.out.print("out 0, ");
+      			 	}
+      			 	else{
+      			 		out_num = out_num - 4;
+      			 		System.out.print("out " + Integer.toString(out_num) + ", ");
+      			 	}
 
-  			 System.out.println();
+             generate_interval generate = new generate_interval(function);
+             generate.generate();
+             //generate.readmap();
 
-         generate_interval generate = new generate_interval(function);
-         generate.generate();
-         generate.readmap();
+             LinearScanAllocation lsa = new LinearScanAllocation(generate.interval);
+             //lsa.readMap();
+             lsa.LinearScanRegisterAllocation();
+             lsa.getMap();
+             allocate_map = new HashMap<>(lsa.allocate_map);
 
-         LinearScanAllocation lsa = new LinearScanAllocation(generate.interval);
-         lsa.readmap();
-  			 
-  			 }
-  			 
+             // for(Map.Entry<String, String> tmp : allocate_map.entrySet()){
+             //    System.out.print(tmp.getKey() + " ");
+             //    System.out.print(tmp.getValue());
+             //    System.out.println();
+             //  }
 
-  			//int in = function.stack.in;
-  			//int out = function.stack.out;
-  			//int local = function.stack.local;
+             System.out.print("local " + Integer.toString(lsa.local) + "]");
+             System.out.println();
+             //add a[]
+             //add local[]
 
-  			//System.out.println("[in" + Integer.toString(in) + " out " + Integer.toString(out) + " local" + Integer.toString(local) + "]");
+             LinkedList<String> label_instruction = new LinkedList<>();
+             LinkedList<Integer> label_line = new LinkedList<>();
+
+             for(VCodeLabel tmp : function.labels){
+                label_instruction.add(tmp.ident + ":");
+                label_line.add(tmp.sourcePos.line);
+             }
+
+             for(VInstr instruction : function.body){
+                int current_line = instruction.sourcePos.line;
+                while(!label_line.isEmpty() && current_line > label_line.getFirst()){
+                  System.out.println(label_instruction.getFirst());
+                  label_line.pop();
+                  label_instruction.pop();
+                }
+                instruction.accept(v2vm);
+             }
+    			 
+    			//int in = function.stack.in;
+    			//int out = function.stack.out;
+    			//int local = function.stack.local;
+
+    			//System.out.println("[in" + Integer.toString(in) + " out " + Integer.toString(out) + " local" + Integer.toString(local) + "]");
 
 
-  			
+    			
 
-  		}
+    		}
+      }
 		
 	
 
@@ -100,11 +129,36 @@ public class V2VM extends VInstr.Visitor<Throwable>{
 	}
 
 	public void visit(VAssign v) throws Throwable{
+      String source = v.source.toString();
+      if(allocate_map.get(source) == null){
+          System.out.println(allocate_map.get(v.dest.toString()) + " = " + source);
+      }
 
+      else{
+          System.out.println(allocate_map.get(v.dest.toString()) + " = " + allocate_map.get(source));
+      }
 	}
 
 	public void visit(VBuiltIn v) throws Throwable{
 
+      if(v.dest != null)
+          System.out.print(allocate_map.get(v.dest.toString()) + " = ");
+
+      System.out.print(v.op.name + "(");
+
+      for(VOperand tmp : v.args){
+          String source = tmp.toString();
+          if(allocate_map.get(source) == null){
+              System.out.print(source + " ");
+          }
+
+          else{
+              System.out.print(allocate_map.get(source) + " ");
+          }
+      }
+
+      System.out.print(")");
+      System.out.println();
 	}
 
 	public void visit(VMemWrite v) throws Throwable{
@@ -124,7 +178,20 @@ public class V2VM extends VInstr.Visitor<Throwable>{
 	}
 
 	public void visit(VReturn v) throws Throwable{
+      System.out.print("ret ");
 
+      if(v.value != null){
+          String source = v.value.toString();
+          if(allocate_map.get(source) == null){
+              System.out.print(source);
+          }
+
+          else{
+              System.out.print(allocate_map.get(source));
+          }
+      }
+
+      System.out.println();
 	}
 }
 
